21470556e8f59bc00d283f7ba79d34164c7be793
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-12-01 18:42:26 -0800

    add log level to logger in databaseHelpers

--- src/helpers/__test__/databaseHelpers.test.js
+++ src/helpers/__test__/databaseHelpers.test.js
@@ -9,11 +9,11 @@ describe( 'databaseHelpers', () => {
                 db.authenticate = jest.fn().mockName( 'authenticate' );
                 db.sync = jest.fn().mockName( 'sync' );
             } );
-            it ( 'db.authenticate only once', async() => {
+            it( 'db.authenticate only once', async() => {
                 await databaseHelpers.setupDB();
                 expect( db.authenticate ).toHaveBeenCalledTimes( 1 );
             } );
-            it ( 'db.sync only once', async() => {
+            it( 'db.sync only once', async() => {
                 await databaseHelpers.setupDB();
                 expect( db.sync ).toHaveBeenCalledTimes( 1 );
             } );
@@ -27,21 +27,25 @@ describe( 'databaseHelpers', () => {
             it( 'db.authenticate throws error', async() => {
                 const authenticateError = new Error( 'authenticate error' );
                 db.authenticate = jest.fn()
-                    .mockImplementation( () => {throw authenticateError;} ).mockName( 'authenticate throws' );
+                    .mockImplementation( () => {
+                        throw authenticateError;
+                    } ).mockName( 'authenticate throws' );
                 
                 await databaseHelpers.setupDB();
                 expect( logger.log ).toHaveBeenCalledTimes( 1 );
-                expect( logger.log ).toHaveBeenCalledWith( authenticateError );
+                expect( logger.log ).toHaveBeenCalledWith( 'error', authenticateError );
             } );
             it( 'db.sync throws Error', async() => {
                 const syncError = new Error( 'sync error' );
                 db.sync = jest.fn().mockName( 'sync throws' );
                 db.authenticate = jest.fn()
-                    .mockImplementation( () => {throw syncError;} ).mockName( 'sync throws' );
+                    .mockImplementation( () => {
+                        throw syncError;
+                    } ).mockName( 'sync throws' );
                 
                 await databaseHelpers.setupDB();
                 expect( logger.log ).toHaveBeenCalledTimes( 1 );
-                expect( logger.log ).toHaveBeenCalledWith( syncError );
+                expect( logger.log ).toHaveBeenCalledWith( 'error', syncError );
             } );
         } );
     } );
